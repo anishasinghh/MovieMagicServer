a1e4caf6742d4d8347beb951c514a570754d0861
arunimag23 <emailarunimagupta@gmail.com>
2023-12-06 22:33:52 -0500

    find by imdb

--- movies/dao.js
+++ movies/dao.js
@@ -2,8 +2,12 @@ import model from "./model.js";
 export const createMovie = (movie) => model.create(movie);
 export const findAllMovies = () => model.find();
 export const findMovieById = (movieId) => model.findById(movieId);
+export const findMovieByIMDB = (imdbId) => model.findOne({ imdbId: imdbId });
 export const findMovieByTitle = (title) =>
   model.findOne({ title: title });
 export const updateMovie = (movieId, movie) =>
   model.updateOne({ _id: movieId }, { $set: movie });
+export const incrementLikes = (movieId) => {
+  model.findOneAndUpdate({ id: movieId }, { $inc: { likes: 1 } })
+};
 export const deleteMovie = (movieId) => model.deleteOne({ _id: movieId });
--- movies/routes.js
+++ movies/routes.js
@@ -8,7 +8,7 @@ function MovieRoutes(app) {
   const deleteMovie = async (req, res) => {
     const status = await dao.deleteMovie(req.params.movieId);
     res.json(status);
-};
+  };
   const findAllMovies = async (req, res) => {
     const movies = await dao.findAllMovies();
     res.json(movies);
@@ -17,17 +17,32 @@ function MovieRoutes(app) {
     const movie = await dao.findMovieById(req.params.movieId);
     res.json(movie);
   };
+  const findMovieByIMDB = async (req, res) => {
+    console.log("reached in route")
+    console.log("passed in params " + req.params);
+    const movie = await dao.findMovieByIMDB(req.params);
+    res.json(movie);
+  };
   const updateMovie = async (req, res) => {
     const { movieId } = req.params;
     const status = await dao.updateMovie(movieId, req.body);
     const currentMovie = await dao.findMovieById(movieId);
-    req.session['currentMovie'] = currentMove;
+    req.session['currentMovie'] = currentMovie;
     res.json(status);
   };
+  // const updateLikes = async (req, res) => {
+  //   const { movieId } = req.params;
+  //   const status = await dao.updateMovie(movieId, req.body);
+  //   const currentMovie = await dao.incrementLikes(movieId);
+  //   req.session['currentMovie'] = currentMovie;
+  //   res.json(status);
+  // };
+
 
   app.post("/api/movies", createMovie);
   app.get("/api/movies", findAllMovies);
   app.get("/api/movies/:movieId", findMovieById);
+  app.get("/api/movies/:imdbID", findMovieByIMDB);
   app.put("/api/movies/:movieId", updateMovie);
   app.delete("/api/movies/:movieId", deleteMovie);
 }
